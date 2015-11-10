de0e114746c054595be64448008b0d28c46910ae
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-10 15:52:53 +0900

    rootをログインページに

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :destroy]
+
+  root 'sessions#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
