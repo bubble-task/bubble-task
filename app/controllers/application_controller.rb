fd776c2d239a921a54e56a5547c53c098d1fc126
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-11 18:05:05 +0900

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,14 @@
 class ApplicationController < ActionController::Base
-
-  def sign_in(user)
-    manager = SessionManager.new(session)
-    manager.sign_in(user)
-  end
-
-  def current_user
-    manager = SessionManager.new(session)
-    manager.current_user
-  end
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  delegate :sign_in, :current_user, to: :session_manager
   helper_method :current_user
+
+  private
+
+    def session_manager
+      @session_manager ||= SessionManager.new(session)
+    end
 end
--- app/controllers/concerns/session_manager.rb
+++ app/controllers/concerns/session_manager.rb
@@ -11,5 +11,4 @@ class SessionManager
   def current_user
     User.find(@session[:user_id])
   end
-
 end
