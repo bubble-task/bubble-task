56d5b4f7b1d7782108a1dfb31bd7946bd02aabbd
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-11 13:53:45 +0900

    fix

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,35 +1,52 @@
 require 'rails_helper'
 
 describe User do
-  it 'OAuthユーザの情報から新規のユーザを作成する' do
-    auth_hash = {
-      'provider' => 'google',
-      'uid' => '1234567890',
-      'info' => {
-        'email' => 'user@gaiax.com',
-        'name' => 'TestUser'
+  describe 'OAuthユーザの情報から新規のユーザを作成する' do
+    it do
+      auth_hash = {
+        'provider' => 'google',
+        'uid' => '1234567890',
+        'info' => {
+          'email' => 'user@gaiax.com',
+          'name' => 'TestUser'
+        }
       }
-    }
-    user = User.create_from_oauth_user(auth_hash)
-    expect(user.provider).to eq auth_hash['provider']
-    expect(user.uid).to eq auth_hash['uid']
-    expect(user.email).to eq auth_hash['info']['email']
-    expect(user.name).to eq auth_hash['info']['name']
+      user = User.create_from_oauth_user(auth_hash)
+      expect(user.provider).to eq auth_hash['provider']
+      expect(user.uid).to eq auth_hash['uid']
+      expect(user.email).to eq auth_hash['info']['email']
+      expect(user.name).to eq auth_hash['info']['name']
+    end
   end
 
-  it 'OAuthユーザの情報から既存のユーザを取得する' do
-    auth_hash = {
-      'provider' => 'google',
-      'uid' => '1234567890',
-      'info' => {
-        'email' => 'user@gaiax.com',
-        'name' => 'TestUser'
-      }
-    }
-    User.create_from_oauth_user(auth_hash)
-    created_user = User.last
+  describe 'OAuthユーザの情報からユーザを取得する' do
+    context '既にユーザが登録されている場合' do
+      it do
+        auth_hash = {
+          'provider' => 'google',
+          'uid' => '1234567890',
+          'info' => {
+            'email' => 'user@gaiax.com',
+            'name' => 'TestUser'
+          }
+        }
+        created_user = User.create_from_oauth_user(auth_hash)
+        user = User.find_by_oauth_credential(auth_hash['provider'], auth_hash['uid'])
+        expect(user).to eq(created_user)
+      end
+    end
 
-    user = User.find_by_oauth_credential(auth_hash['provider'], auth_hash['uid'])
-    expect(user).to eq(created_user)
+    skip 'ユーザが登録されていない場合' do
+      it do
+        auth_hash = {
+          'provider' => 'google',
+          'uid' => '1234567890',
+          'info' => {
+            'email' => 'user@gaiax.com',
+            'name' => 'TestUser'
+          }
+        }
+      end
+    end
   end
 end
