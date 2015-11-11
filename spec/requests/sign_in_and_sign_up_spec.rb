7caeb69e7bd852d235088eb46386b71103e7fca9
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2015-11-11 17:03:25 +0900

    不要なテストを削除

--- spec/requests/sign_in_and_sign_up_spec.rb
+++ spec/requests/sign_in_and_sign_up_spec.rb
@@ -19,23 +19,5 @@ describe 'GET /auth/google/callback' do
       follow_redirect!
       expect(response.body).to include 'TestUser'
     end
-
-    it 'ログイン状態になっていること' do
-      OmniAuth.config.add_mock(
-        :google,
-        {
-          'provider' => 'google',
-          'uid' => '1234567890',
-          'info' => {
-            'email' => 'user@gaiax.com',
-            'name' => 'The User'
-          }
-        }
-      )
-      Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
-      get(oauth_callbacks_path(provider: :google))
-      follow_redirect!
-      expect(response.body).to include 'The User'
-    end
   end
 end
