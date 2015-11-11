9f31b74db2a785b623b40a3056a38c0f4da232eb
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2015-11-11 19:23:32 +0900

    規約の違反を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def authorize!
-      unless current_user
-        redirect_to new_session_url
-      end
+      redirect_to new_session_url unless current_user
     end
 
     def sign_in(user)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 describe User do
+  let(:auth_hash) do
+    {
+      'provider' => 'google',
+      'uid' => '1234567890',
+      'info' => {
+        'email' => 'user@gaiax.com',
+        'name' => 'TestUser',
+      },
+    }
+  end
   describe 'OAuthユーザの情報から新規のユーザを作成する' do
     it do
-      auth_hash = {
-        'provider' => 'google',
-        'uid' => '1234567890',
-        'info' => {
-          'email' => 'user@gaiax.com',
-          'name' => 'TestUser'
-        }
-      }
       user = User.create_from_oauth_user(auth_hash)
       expect(user.provider).to eq auth_hash['provider']
       expect(user.uid).to eq auth_hash['uid']
@@ -22,14 +24,6 @@ describe User do
   describe 'OAuthユーザの情報からユーザを取得する' do
     context '既にユーザが登録されている場合' do
       it do
-        auth_hash = {
-          'provider' => 'google',
-          'uid' => '1234567890',
-          'info' => {
-            'email' => 'user@gaiax.com',
-            'name' => 'TestUser'
-          }
-        }
         created_user = User.create_from_oauth_user(auth_hash)
         user = User.find_by_oauth_credential(auth_hash['provider'], auth_hash['uid'])
         expect(user).to eq(created_user)
@@ -38,14 +32,6 @@ describe User do
 
     context 'ユーザが登録されていない場合' do
       it do
-        auth_hash = {
-          'provider' => 'google',
-          'uid' => '1234567890',
-          'info' => {
-            'email' => 'user@gaiax.com',
-            'name' => 'TestUser'
-          }
-        }
         user = User.find_by_oauth_credential(auth_hash['provider'], auth_hash['uid'])
         expect(user).to be_nil
       end
