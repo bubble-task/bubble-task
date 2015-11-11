d7b414b08a9992e583930dfabc5f51b6f84c3efb
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-11 14:23:50 +0900

    不要な依存性の注入を削除

--- app/models/user_repository.rb
+++ app/models/user_repository.rb
@@ -1,12 +1,8 @@
 class UserRepository
 
-  def initialize(user)
-    @user = user
-  end
-
   def find_by_oauth_credential(auth_hash)
-    user = @user.find_by_oauth_credential(auth_hash['provider'], auth_hash['uid'])
+    user = User.find_by_oauth_credential(auth_hash['provider'], auth_hash['uid'])
     return user if user
-    @user.create_from_oauth_user(auth_hash)
+    User.create_from_oauth_user(auth_hash)
   end
 end
--- spec/models/user_repository_spec.rb
+++ spec/models/user_repository_spec.rb
@@ -1,35 +1,18 @@
 require 'rails_helper'
 
-class UserSpy
-
-  attr_accessor :auth_hash
-
-  def initialize(user = nil)
-    @user = user
-  end
-
-  def find_by_oauth_credential(provider, uid)
-    @user
-  end
-
-  def create_from_oauth_user(auth_hash)
-    self.auth_hash = auth_hash
-  end
-end
-
 describe UserRepository do
   context 'ユーザが登録されている場合' do
     it '登録されているユーザを返す' do
       auth_hash = default_auth_hash
       expected_user = create_user_from_oauth_credential(auth_hash)
-      user = UserRepository.new(User).find_by_oauth_credential(auth_hash)
+      user = UserRepository.new.find_by_oauth_credential(auth_hash)
       expect(user).to eq(expected_user)
     end
   end
 
   context 'ユーザがまだ登録されていない場合' do
     it 'Userに新規のユーザを作成させる' do
-      user = UserRepository.new(User).find_by_oauth_credential(default_auth_hash)
+      user = UserRepository.new.find_by_oauth_credential(default_auth_hash)
       expected_user = User.last
       expect(user).to eq(expected_user)
     end
