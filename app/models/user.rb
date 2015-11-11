6cdb9e77d7261e24534f7c212d21e19533e5cea6
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-11 13:34:16 +0900

    delegate toを使うようリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,11 @@
 class User < ActiveRecord::Base
   has_one :oauth_credential
 
+  delegate :provider, :uid, to: :oauth_credential
+
   def self.create_from_oauth_user(auth_hash)
     user = new(name: auth_hash['info']['name'], email: auth_hash['info']['email'])
     user.build_oauth_credential(provider: auth_hash['provider'], uid: auth_hash['uid'])
     user.save
-  end
-
-  def provider
-    oauth_credential.provider
-  end
-
-  def uid
-    oauth_credential.uid
   end
 end
