6cc2bc5f5e898b396a6c0119a791bf8f1320ade9
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-16 19:11:20 +0900

    add spec for OauthCallbacksController

---
+++ spec/controllers/oauth_callbacks_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe OauthCallbacksController do
+  describe '#create' do
+    subject do
+      OmniAuth.config.add_mock(:google, default_auth_hash)
+      request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
+
+      get :create, provider: :google
+      response
+    end
+
+    context 'ログインしていない場合' do
+      it { is_expected.to redirect_to(root_url) }
+    end
+
+    context 'ログインしている場合' do
+      before { sign_in }
+      it { is_expected.to redirect_to(root_url) }
+    end
+  end
+end
