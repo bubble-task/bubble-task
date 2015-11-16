877c3fbb8718f760b61620018c38cb157bcc9298
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-16 18:40:10 +0900

    refactor

--- spec/support/sign_in_helper.rb
+++ spec/support/sign_in_helper.rb
@@ -3,23 +3,26 @@ require_relative './user_helper'
 module SignInHelper
   include UserHelper
 
-  def oauth_sign_in(user = { email: nil, name: nil })
+  def oauth_sign_in(provider = :google, user = { email: nil, name: nil })
     auth_hash = default_auth_hash.merge(user)
-    OmniAuth.config.add_mock(:google, auth_hash)
-    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
-    visit oauth_callbacks_path(provider: :google)
+    setup_omniauth_mock(provider, auth_hash)
+    visit oauth_callbacks_path(provider: provider)
   end
 
-  def request_oauth_sign_in(user = { email: nil, name: nil })
+  def request_oauth_sign_in(provider = :google, user = { email: nil, name: nil })
     auth_hash = default_auth_hash.merge(user)
-    OmniAuth.config.add_mock(:google, auth_hash)
-    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
-    get oauth_callbacks_path(provider: :google)
+    setup_omniauth_mock(provider, auth_hash)
+    get oauth_callbacks_path(provider: provider)
     follow_redirect!
   end
 
   def sign_in(user_params = { email: nil, name: nil })
     user = User.new(default_auth_hash['info'].merge(user_params))
     allow(controller).to receive(:current_user) { user }
+  end
+
+  def setup_omniauth_mock(provider = :google, auth_hash)
+    OmniAuth.config.add_mock(provider, auth_hash)
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[provider]
   end
 end
