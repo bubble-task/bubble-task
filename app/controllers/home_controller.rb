73b3a9b9311f82d1849734659930b9a2cacbaff8
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-17 16:32:40 +0900

    refactor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   before_action :authorize!
 
   def index
-    @tasks = Task.where(author_id: current_user.id)
+    @tasks = Task.by_author(current_user)
   end
 end
