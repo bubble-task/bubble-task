4ea8bc40be411a36018aa498c4f63e17b956e432
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-17 14:56:25 +0900

    revert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    SessionManager.current_user(session) || User.new(name: '末吉哉')
+    SessionManager.current_user(session)
   end
 
   def signed_in?
