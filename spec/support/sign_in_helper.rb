40d84c8e6f571e197a3b70a36eaeabea5e07e93d
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-17 17:05:56 +0900

    fix

--- spec/support/sign_in_helper.rb
+++ spec/support/sign_in_helper.rb
@@ -3,13 +3,20 @@ require_relative './user_helper'
 module SignInHelper
   include UserHelper
 
-  def oauth_sign_in(auth_hash: auth_hash, provider: :google)
-    setup_omniauth_mock(provider, auth_hash)
+  def oauth_sign_in(auth_hash: nil, provider: :google)
+    credential = if auth_hash
+                   auth_hash
+                 else
+                   generate_auth_hash.tap do |h|
+                     create_user_from_oauth_credential(h)
+                   end
+                 end
+    setup_omniauth_mock(provider, credential)
     visit oauth_callbacks_path(provider: provider)
   end
 
-  def request_oauth_sign_in(provider = :google, user = { email: nil, name: nil })
-    auth_hash = generate_auth_hash.merge(user)
+  def request_oauth_sign_in(auth_hash: auth_hash, provider: :google)
+    auth_hash ||= generate_auth_hash
     setup_omniauth_mock(provider, auth_hash)
     get oauth_callbacks_path(provider: provider)
     follow_redirect!
