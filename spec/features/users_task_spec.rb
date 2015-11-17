fc3b8e7eb825edcb8656bfc7e8dfa117d8c40c59
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-17 17:06:31 +0900

    fix

--- spec/features/users_task_spec.rb
+++ spec/features/users_task_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe '自身が作成したタスクの一覧' do
   it do
     user_a_auth_hash = generate_auth_hash
-    user_a = create_user_from_oauth_credential(generate_auth_hash)
+    user_a = create_user_from_oauth_credential(user_a_auth_hash)
     user_b = create_user_from_oauth_credential(generate_auth_hash)
 
     user_b_task = user_b.create_task('ユーザBのタスク').tap(&:save)
