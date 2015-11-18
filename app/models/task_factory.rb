d221756a7efa80ec50accf175aafdab983bab4ea
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-18 12:05:33 +0900

    refactor

--- app/models/task_factory.rb
+++ app/models/task_factory.rb
@@ -1,10 +1,28 @@
 class TaskFactory
 
   def self.create(author_id, title, description = nil, tags = [])
-    task = Task.new(author_id: author_id, title: title)
-    task.tagging(tags)
-    return task unless description
-    task.write_description(description)
-    task
+    new(author_id, title).build do
+      set_description(description)
+      set_tags(tags)
+    end
+  end
+
+  def initialize(author_id, title)
+    @task = Task.new(author_id: author_id, title: title)
+  end
+
+  def build(&block)
+    instance_eval(&block)
+    @task
+  end
+
+  def set_description(description)
+    return unless description
+    @task.write_description(description)
+  end
+
+  def set_tags(tags)
+    return if tags.empty?
+    @task.tagging(tags)
   end
 end
