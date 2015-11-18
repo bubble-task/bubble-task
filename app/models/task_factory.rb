6758c9ff159f6121a028164c0b7f622a2644f312
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-18 12:19:05 +0900

    fix

--- app/models/task_factory.rb
+++ app/models/task_factory.rb
@@ -1,9 +1,9 @@
 class TaskFactory
 
   def self.create(author_id, title, description = nil, tags = [])
-    new(author_id, title).build do
-      set_description(description)
-      set_tags(tags)
+    new(author_id, title).with do
+      write_description(description)
+      tagging(tags)
     end
   end
 
@@ -11,17 +11,17 @@ class TaskFactory
     @task = Task.new(author_id: author_id, title: title)
   end
 
-  def build(&block)
+  def with(&block)
     instance_eval(&block)
     @task
   end
 
-  def set_description(description)
+  def write_description(description)
     return unless description
     @task.write_description(description)
   end
 
-  def set_tags(tags)
+  def tagging(tags)
     return if tags.empty?
     @task.tagging(tags)
   end
