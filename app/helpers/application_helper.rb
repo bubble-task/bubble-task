196e1b9b5fdcf9678177fa2e9fe95943908a9546
haazime <h4zime@gmail.com>
2015-11-22 21:24:54 +0900

    activate if current page is active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def activate_menu(actives)
+    ' class=active'.html_safe if detect_active_page(actives)
+  end
+
+  def detect_active_page(actives)
+    actives
+      .map(&:flatten)
+      .detect { |(c, a)| current_page?(controller: c, action: a) }
+  end
 end
