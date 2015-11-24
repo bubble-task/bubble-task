7b13848511648dfbc419e3efdb9e77a5cfe2b2c7
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-24 17:33:14 +0900

    refactor

--- spec/features/tag_index_spec.rb
+++ spec/features/tag_index_spec.rb
@@ -1,24 +1,30 @@
 require 'rails_helper'
 
 describe 'タグの一覧画面' do
+  before do
+    create_user_from_oauth_credential(auth_hash)
+    oauth_sign_in(auth_hash: auth_hash)
+  end
+
+  def create_task(*args)
+    TaskFactory
+      .create(*args)
+      .tap(&:save)
+  end
+
   let(:auth_hash) { generate_auth_hash }
 
+  let(:tags) { all('.tag').map(&:text) }
+
   it do
-    user = create_user_from_oauth_credential(auth_hash)
-    oauth_sign_in(auth_hash: auth_hash)
-    task = user.create_task('タスクのタイトル', nil, ['タグ'])
-    task.save
+    create_task(1, 'タスクのタイトル', nil, ['タグ'])
     visit tags_path
-    expect(page).to have_content('タグ')
+    expect(tags).to eq(%w(タグ))
   end
 
   it do
-    user = create_user_from_oauth_credential(auth_hash)
-    oauth_sign_in(auth_hash: auth_hash)
-    task = user.create_task('タスクのタイトル', nil, %w(タグB タグC タグA))
-    task.save
+    create_task(1, 'タスクのタイトル', nil, %w(タグB タグC タグA))
     visit tags_path
-    tags = all('.tag').map(&:text)
     expect(tags).to eq(%w(タグA タグB タグC))
   end
 end
