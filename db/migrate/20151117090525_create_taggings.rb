3b12ebde55e2f24b662bda9b65894d93ced89eb0
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-25 14:16:44 +0900

    add collation for create table

--- db/migrate/20151117090525_create_taggings.rb
+++ db/migrate/20151117090525_create_taggings.rb
@@ -2,7 +2,7 @@ class CreateTaggings < ActiveRecord::Migration
   def change
     create_table :taggings do |t|
       t.references :task, null: false
-      t.string :tag, null: false
+      t.string :tag, null: false, collation: 'ja_JP.UTF-8'
     end
   end
 end
