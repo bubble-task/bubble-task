43c1438bf22bbf8034cd76ba6bc68986087ab17a
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-25 15:15:12 +0900

    for travis

--- db/migrate/20151117090525_create_taggings.rb
+++ db/migrate/20151117090525_create_taggings.rb
@@ -1,8 +1,10 @@
 class CreateTaggings < ActiveRecord::Migration
   def change
+    tag_column_spec = { null: false }
+    tag_column_spec.merge!(collation: 'ja_JP.UTF-8') unless ENV['CI']
     create_table :taggings do |t|
       t.references :task, null: false
-      t.string :tag, null: false, collation: 'ja_JP.UTF-8'
+      t.string :tag, tag_column_spec
     end
   end
 end
