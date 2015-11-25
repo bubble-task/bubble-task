b237d343f58984d6bec610b29de4411fc4a84c8a
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-25 19:15:00 +0900

    avoid N+1 query

--- app/models/task_repository.rb
+++ app/models/task_repository.rb
@@ -2,6 +2,6 @@ module TaskRepository
   module_function
 
   def all_by_tag(tag)
-    Task.joins(:taggings).where(taggings: { tag: tag })
+    Task.includes(:taggings).where(taggings: { tag: tag })
   end
 end
