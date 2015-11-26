545146ccfdf13850ba0ba65ea96712b932fa0d3b
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-26 14:12:01 +0900

    fix tag tasks title

--- spec/features/tag_tasks_index_spec.rb
+++ spec/features/tag_tasks_index_spec.rb
@@ -21,13 +21,13 @@ describe 'タスクのタグからタグに紐づくタスクの一覧ページ�
   describe '各一覧画面からの遷移' do
     before { task }
     let(:task) { create_task(user.id, 'タスクのタイトル', nil, %w(タグ)) }
-    let(:tag_at_header) { first('.tag-tasks-header .tag').text }
+    let(:tag) { find('nav').text.sub(/のタスク/, '') }
 
     context 'タスク一覧のタグをクリックした場合' do
       it do
         visit root_path
         click_link('タグ')
-        expect(tag_at_header).to eq('タグ')
+        expect(tag).to eq('タグ')
       end
     end
 
@@ -35,7 +35,7 @@ describe 'タスクのタグからタグに紐づくタスクの一覧ページ�
       it do
         visit tags_path
         click_link('タグ')
-        expect(tag_at_header).to eq('タグ')
+        expect(tag).to eq('タグ')
       end
     end
 
@@ -43,7 +43,7 @@ describe 'タスクのタグからタグに紐づくタスクの一覧ページ�
       it do
         visit task_path(task)
         click_link('タグ')
-        expect(tag_at_header).to eq('タグ')
+        expect(tag).to eq('タグ')
       end
     end
   end
