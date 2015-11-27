c559cd2d2617f989d9ce96dcabbfe67fd0021701
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-11-27 18:01:16 +0900

    refactor

--- spec/features/validation_for_task_creation_spec.rb
+++ spec/features/validation_for_task_creation_spec.rb
@@ -6,49 +6,70 @@ describe 'タスク作成時にバリデーションをかける' do
   end
 
   let(:valid_title) { 'タスクのタイトル' }
-  let(:out_of_boundary_length) { 17 }
-  let(:inside_of_boundary_length) { 16 }
 
-  it do
-    create_task_from_ui('')
-    expect(page).to have_content 'タイトルを入力してください'
+  describe 'タイトル' do
+    subject do
+      create_task_from_ui(title)
+      page
+    end
+
+    context '未入力' do
+      let(:title) { '' }
+      it { is_expected.to have_content 'タイトルを入力してください' }
+    end
+
+    context '最大文字数入力' do
+      let(:title) { 'a' * 40 }
+      it { is_expected.to have_link(title) }
+    end
+
+    context '最大文字数+1入力' do
+      let(:title) { 'a' * 41 }
+      it { is_expected.to have_content 'タイトルは40文字以内で入力してください' }
+    end
   end
 
-  it do
-    title = 'a' * 40
-    create_task_from_ui('a' * 40)
-    expect(page).to have_link(title)
+  describe '説明' do
+    subject do
+      create_task_from_ui(valid_title, description)
+      page
+    end
+
+    context '最大文字数入力' do
+      let(:description) { 'a' * 5000 }
+      it { is_expected.to have_link(valid_title) }
+    end
+
+    context '最大文字数+1入力' do
+      let(:description) { 'a' * 5001 }
+      it { is_expected.to have_content '説明は5000文字以内で入力してください' }
+    end
   end
 
-  it do
-    create_task_from_ui('a' * 41)
-    expect(page).to have_content 'タイトルは40文字以内で入力してください'
-  end
+  describe 'タグ' do
+    let(:out_of_boundary_length) { 17 }
+    let(:inside_of_boundary_length) { 16 }
 
-  it do
-    create_task_from_ui(valid_title, 'a' * 5001)
-    expect(page).to have_content '説明は5000文字以内で入力してください'
-  end
+    subject do
+      create_task_from_ui(valid_title, nil, tag_words)
+      page
+    end
 
-  it do
-    create_task_from_ui(valid_title, 'a' * 5000)
-    expect(page).to have_link(valid_title)
-  end
+    context '全て最大文字数以内で入力' do
+      let(:tag_words) { "#{'a' * 8} #{'b' * 9}" }
+      it { is_expected.to have_link('タスクのタイトル') }
+    end
 
-  it do
-    tag_words = 'a' * out_of_boundary_length
-    create_task_from_ui(valid_title, nil, tag_words)
-    expect(page).to have_content("タグ「#{tag_words}」は16文字以内で入力してください")
-  end
+    context '最大文字数+1を単独で入力' do
+      let(:tag_words) { 'a' * out_of_boundary_length }
+      it { is_expected.to have_content("タグ「#{tag_words}」は16文字以内で入力してください") }
+    end
 
-  it do
-    invalid_tag = 'b' * out_of_boundary_length
-    create_task_from_ui(valid_title, nil, "#{'a' * inside_of_boundary_length} #{invalid_tag}")
-    expect(page).to have_content("タグ「#{invalid_tag}」は16文字以内で入力してください")
-  end
+    context '最大文字数+1と最大文字数を一つずつ入力' do
+      let(:invalid_tag) { 'b' * out_of_boundary_length }
+      let(:tag_words) { "#{'a' * inside_of_boundary_length} #{invalid_tag}" }
 
-  it do
-    create_task_from_ui(valid_title, nil, "#{'a' * 8} #{'b' * 9}")
-    expect(page).to have_link('タスクのタイトル')
+      it { is_expected.to have_content("タグ「#{invalid_tag}」は16文字以内で入力してください") }
+    end
   end
 end
