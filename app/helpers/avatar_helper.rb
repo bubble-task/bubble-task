f15ba38a7b3de89c14883c8385be4cdcf731d2ef
haazime <h4zime@gmail.com>
2015-12-06 16:21:14 +0900

    fix

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,5 +1,4 @@
 module AvatarHelper
-
   def show_avatar_image(user, force: false)
     return if Rails.env.test? && !force
     javascript_tag("$('#current-user-avatar').attr({ src: Gravtastic('#{user.email}') })").html_safe
