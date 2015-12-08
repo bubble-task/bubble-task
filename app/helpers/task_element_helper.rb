d54f72dc6dc69380f43b5cc5140c3b59f4df414f
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-12-08 15:15:06 +0900

    TaskPresenter#task_completion_state_idを削除

--- app/helpers/task_element_helper.rb
+++ app/helpers/task_element_helper.rb
@@ -2,4 +2,8 @@ module TaskElementHelper
   def task_summary_css_id(task_id)
     "task_#{task_id}"
   end
+
+  def task_completion_css_id(task_id)
+    "task_#{task_id}_completion_state"
+  end
 end
--- spec/helpers/task_element_helper_spec.rb
+++ spec/helpers/task_element_helper_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 describe TaskElementHelper do
-  describe '#css_id' do
+  let(:view) { Class.new.extend(described_class) }
+
+  describe '#task_summary_css_id' do
     it do
-      v = Class.new.extend(described_class)
-      r = v.task_summary_css_id(123)
+      r = view.task_summary_css_id(123)
       expect(r).to eq('task_123')
+    end
+  end
+
+  describe '#task_completion_css_id' do
+    it do
+      r = view.task_completion_css_id(123)
+      expect(r).to eq('task_123_completion_state')
     end
   end
 end
