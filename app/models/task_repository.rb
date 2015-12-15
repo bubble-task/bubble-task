ed8e5febf571598b6b8adbf5b188d76485bfd33b
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-12-15 16:49:40 +0900

    不要なメソッドを削除

--- app/models/task_repository.rb
+++ app/models/task_repository.rb
@@ -2,11 +2,15 @@ module TaskRepository
   class << self
 
     def find_by_id(task_id)
-      task_with_taggings.find_by(id: task_id)
+      Task.includes(:taggings, { assignments: :user })
+        .find_by(id: task_id)
     end
 
-    def all_by_author_id(author_id)
-      task_with_taggings.where(author_id: author_id).order(:id)
+    def all_uncompleted_by_author_id(author_id)
+      Task.includes(:completed_task, :taggings, { assignments: :user })
+        .where(author_id: author_id)
+        .where(completed_tasks: { id: nil })
+        .order(:id)
     end
 
     def all_by_tag(tag)
@@ -18,18 +22,5 @@ module TaskRepository
         .where(completed_tasks: { id: nil })
         .order(:id)
     end
-
-    def all_uncompleted_by_author_id(author_id)
-      Task.includes(:completed_task, :taggings, { assignments: :user })
-        .where(author_id: author_id)
-        .where(completed_tasks: { id: nil })
-        .order(:id)
-    end
-
-    private
-
-      def task_with_taggings
-        Task.includes(:taggings, { assignments: :user })
-      end
   end
 end
