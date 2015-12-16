923076e63dfd0abfa71a78fe0d1db60e9c2c9986
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2015-12-16 17:19:55 +0900

    期間を絞り込む機能を実装

--- app/models/task_repository.rb
+++ app/models/task_repository.rb
@@ -18,8 +18,9 @@ module TaskRepository
                .where(author_id: author_id)
                .where.not(completed_tasks: { id: nil })
       return base if from_date.nil? && to_date.nil?
-      return base.where('completed_tasks.completed_at >= ?', from_date) if from_date
-      return base.where('completed_tasks.completed_at <= ?', to_date) if to_date
+      base = base.where('completed_tasks.completed_at >= ?', from_date) if from_date
+      return base unless to_date
+      base.where('completed_tasks.completed_at <= ?', to_date)
     end
 
     def all_by_tag(tag)
--- spec/requests/search_achievement_by_term_spec.rb
+++ spec/requests/search_achievement_by_term_spec.rb
@@ -82,4 +82,39 @@ describe 'GET /achievements' do
       expect(tasks).to eq(expected_tasks)
     end
   end
+
+  context '期間の絞り込みの開始日と終了日を指定' do
+    let(:unexpected_tasks) do
+      [
+        create_task(author_id: user.id, title: 'a'),
+        create_task(author_id: user.id, title: 'b').tap do |t|
+          t.complete(Time.zone.parse('2015-11-30'))
+          t.save
+        end,
+        create_task(author_id: user.id, title: 'c').tap do |t|
+          t.complete(Time.zone.parse('2016-01-01'))
+          t.save
+        end
+      ]
+    end
+
+    let(:expected_tasks) do
+      [
+        create_task(author_id: user.id, title: 'd').tap do |t|
+          t.complete(Time.zone.parse('2015-12-01'))
+          t.save
+        end,
+        create_task(author_id: user.id, title: 'e').tap do |t|
+          t.complete(Time.zone.parse('2015-12-31'))
+          t.save
+        end
+      ]
+    end
+
+    it do
+      get achievements_path(from: '2015-12-01', to: '2015-12-31')
+      tasks = assigns(:tasks)
+      expect(tasks).to eq(expected_tasks)
+    end
+  end
 end
