158101ebbf7a480c11679b3668627c2e41274461
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-12-16 16:20:39 +0900

    refactor

--- spec/features/achievement_index_spec.rb
+++ spec/features/achievement_index_spec.rb
@@ -10,19 +10,22 @@ describe '完了したタスクの一覧' do
   let(:user) { create_user_from_oauth_credential(auth_hash) }
   let(:auth_hash) { generate_auth_hash }
 
-  let(:task) { create_task(author_id: user.id, title: 'タスクのタイトル', tags: [tag]) }
-  let(:tag) { 'タグ' }
+  let(:task) { create_task(author_id: user.id, title: 'タスクのタイトル', tags: %w(タグ)) }
+  let(:title_on_page) { first('.task-title') }
+  let(:title_text_on_page) { title_on_page.text }
 
-  it do
-    TaskCompletion.new(task_id: task.id).run
-    visit achievements_path
-    title = first('.task-title').text
-    expect(title).to eq(task.title)
+  context '完了したタスクがある場合' do
+    it do
+      TaskCompletion.new(task_id: task.id).run
+      visit achievements_path
+      expect(title_text_on_page).to eq(task.title)
+    end
   end
 
-  it do
-    visit achievements_path
-    title = first('.task-title')
-    expect(title).to be_nil
+  context '完了したタスクがない場合' do
+    it do
+      visit achievements_path
+      expect(title_on_page).to be_nil
+    end
   end
 end
