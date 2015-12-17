be0a20978cf5173507f76b9b85638f6695ac709e
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-12-17 13:11:36 +0900

    検索結果を完了日時の昇順にならべる

--- app/models/task_repository.rb
+++ app/models/task_repository.rb
@@ -17,6 +17,7 @@ module TaskRepository
       base = Task.includes(:completed_task, :taggings)
              .where(author_id: author_id)
              .where.not(completed_tasks: { id: nil })
+             .order('completed_tasks.completed_at')
       return base if from_datetime.nil? && to_datetime.nil?
       base = base.where('completed_tasks.completed_at >= ?', from_datetime) if from_datetime
       return base unless to_datetime
