bbfaa9031dbdf588f2b52edd1a0a4dc4a13bb5f3
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2015-12-18 16:26:23 +0900

    TaskRepositoryから使用していない関数を削除

--- app/models/task_repository.rb
+++ app/models/task_repository.rb
@@ -13,17 +13,6 @@ module TaskRepository
         .order('tasks.id')
     end
 
-    def all_completed_by_author_id(author_id, from_datetime: nil, to_datetime: nil)
-      base = Task.includes(:completed_task, :taggings)
-             .where(author_id: author_id)
-             .where.not(completed_tasks: { id: nil })
-             .order('completed_tasks.completed_at')
-      return base if from_datetime.nil? && to_datetime.nil?
-      base = base.where('completed_tasks.completed_at >= ?', from_datetime) if from_datetime
-      return base unless to_datetime
-      base.where('completed_tasks.completed_at <= ?', to_datetime)
-    end
-
     def all_by_tag(tag)
       Task
         .includes(:completed_task, { assignments: :user })
