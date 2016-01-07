1022df50c9c9fa66e7b57066822c29f95b909cb1
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-01-07 10:44:35 +0900

    完了タスクのタグ検索を1つのSQLで実行するように変更

--- app/models/criteria/achievement.rb
+++ app/models/criteria/achievement.rb
@@ -13,13 +13,9 @@ module Criteria
 
     def satisfy(relation)
       prepared_relation = @conditions.inject(RelationWrapper.new(relation)) { |r, c| c.prepare(r) }.prepare
-      satisfied_task_ids = @conditions.inject(prepared_relation.select('tasks.id')) { |r, c| c.satisfy(r) }
-      Task
-        .joins(:completed_task)
-        .restrict_by_complated
-        .where(id: satisfied_task_ids.to_a)
-        .order('completed_tasks.completed_at')
-      #default_relation = prepared_relation.restrict_by_complated.order('completed_tasks.completed_at')
+      default_relation = prepared_relation.restrict_by_complated
+      tasks = @conditions.inject(default_relation) { |r, c| c.satisfy(r) }
+      tasks.sort_by(&:completed_at)
     end
   end
 end
