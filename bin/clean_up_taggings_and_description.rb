d6309a59326a3b51c8680ab418169570df54bbb6
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-01-13 14:35:19 +0900

    delete also completed_tasks

--- bin/clean_up_taggings_and_description.rb
+++ bin/clean_up_taggings_and_description.rb
@@ -1,6 +1,18 @@
 task_ids = Task.pluck(:id)
+puts "Tasks: #{task_ids}"
+
 tasks_with_description = TaskDescription.pluck(:task_id)
-tasks_with_tag = Tagging.pluck(:task_id).uniq
-task_ids_for_deleting = (tasks_with_tag + tasks_with_description).uniq - task_ids
+puts "TaskDescriptions: #{tasks_with_description}"
+
+tasks_with_tag = Tagging.pluck(:task_id)
+puts "Taggings: #{tasks_with_tag}"
+
+tasks_with_completion = CompletedTask.pluck(:task_id)
+puts "CompletedTasks: #{tasks_with_completion}"
+
+task_ids_for_deleting = (tasks_with_tag + tasks_with_description + tasks_with_completion).uniq - task_ids
+puts "Target Task ids: #{task_ids_for_deleting}"
+
 TaskDescription.where(task_id: task_ids_for_deleting).destroy_all
 Tagging.where(task_id: task_ids_for_deleting).destroy_all
+CompletedTask.where(task_id: task_ids_for_deleting).destroy_all
