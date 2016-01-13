7243b261789dcf90ab6f97deeb83034fd93b1af8
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-01-13 15:02:04 +0900

    予め設定したURLにのみ戻れる

--- app/controllers/concerns/backward_navigator.rb
+++ app/controllers/concerns/backward_navigator.rb
@@ -1,11 +1,29 @@
 BackwardNavigator = Struct.new(:backward_path) do
+  class << self
 
-  def self.update_backable_path(path)
-    backable_path = if path != '/'
-                      '/'
-                    else
-                      path
-                    end
-    new(backable_path)
+    def register_backable_paths(paths)
+      @backward_paths = paths
+    end
+
+    def backward_paths
+      @backward_paths ||= ['/']
+    end
+
+    def registered_backable_path?(path)
+      backward_paths.include?(path)
+    end
+
+    def default_backward_path
+      backward_paths.first
+    end
+
+    def update_backable_path(path)
+      backable_path = if registered_backable_path?(path)
+                        path
+                      else
+                        default_backward_path
+                      end
+      new(backable_path)
+    end
   end
 end
