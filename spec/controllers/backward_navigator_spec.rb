df760c28ad1f3dfe3bbe26130dc551b2fef440c4
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-01-14 17:54:12 +0900

    refactor

--- spec/controllers/backward_navigator_spec.rb
+++ spec/controllers/backward_navigator_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe BackwardNavigator, type: :model do
+  def ReqPath(string)
+    RequestPath.new(string)
+  end
+
   describe '#backward_path' do
     subject do
       navigator.update_backward_path(current_path)
@@ -10,51 +14,51 @@ describe BackwardNavigator, type: :model do
     let(:navigator) { described_class.new({}, backable_paths) }
 
     context 'register "/", "/tasks" as backable path' do
-      let(:backable_paths) { %w(/ /tasks).map { |p| RequestPath.new(p) } }
+      let(:backable_paths) { %w(/ /tasks).map { |p| ReqPath(p) } }
 
       context 'visit /' do
-        let(:current_path) { RequestPath.new('/') }
+        let(:current_path) { ReqPath('/') }
         it { is_expected.to eq(current_path) }
       end
 
       context 'visit /tasks' do
-        let(:current_path) { RequestPath.new('/tasks') }
+        let(:current_path) { ReqPath('/tasks') }
         it { is_expected.to eq(current_path) }
       end
 
       context 'visit /tasks?tag=ABC' do
-        let(:current_path) { RequestPath.new('/tasks?tag=ABC') }
+        let(:current_path) { ReqPath('/tasks?tag=ABC') }
         it { is_expected.to eq(current_path) }
       end
 
       context 'visit /tasks/new' do
-        let(:current_path) { RequestPath.new('/tasks/new') }
-        it { is_expected.to eq(RequestPath.new('/')) }
+        let(:current_path) { ReqPath('/tasks/new') }
+        it { is_expected.to eq(ReqPath('/')) }
       end
 
       context 'visit / => /tasks/new' do
         it do
-          navigator.update_backward_path(RequestPath.new('/'))
-          navigator.update_backward_path(RequestPath.new('/tasks/new'))
-          expect(navigator.backward_path).to eq(RequestPath.new('/'))
+          navigator.update_backward_path(ReqPath('/'))
+          navigator.update_backward_path(ReqPath('/tasks/new'))
+          expect(navigator.backward_path).to eq(ReqPath('/'))
         end
       end
 
       context 'visit /tasks => /tasks/new => /tasks/new' do
         it do
-          navigator.update_backward_path(RequestPath.new('/tasks'))
-          navigator.update_backward_path(RequestPath.new('/tasks/new'))
-          navigator.update_backward_path(RequestPath.new('/tasks/new'))
-          expect(navigator.backward_path).to eq(RequestPath.new('/tasks'))
+          navigator.update_backward_path(ReqPath('/tasks'))
+          navigator.update_backward_path(ReqPath('/tasks/new'))
+          navigator.update_backward_path(ReqPath('/tasks/new'))
+          expect(navigator.backward_path).to eq(ReqPath('/tasks'))
         end
       end
 
       context 'visit /tasks => / => /tasks/new' do
         it do
-          navigator.update_backward_path(RequestPath.new('/tasks'))
-          navigator.update_backward_path(RequestPath.new('/'))
-          navigator.update_backward_path(RequestPath.new('/tasks/new'))
-          expect(navigator.backward_path).to eq(RequestPath.new('/'))
+          navigator.update_backward_path(ReqPath('/tasks'))
+          navigator.update_backward_path(ReqPath('/'))
+          navigator.update_backward_path(ReqPath('/tasks/new'))
+          expect(navigator.backward_path).to eq(ReqPath('/'))
         end
       end
     end
