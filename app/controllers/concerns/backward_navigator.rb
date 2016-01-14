9753223eff9e750386fb76fba55bd6ca1c9fa795
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-01-14 17:52:31 +0900

    detect backable path with RequestPath Object

--- app/controllers/concerns/backward_navigator.rb
+++ app/controllers/concerns/backward_navigator.rb
@@ -1,10 +1,10 @@
 class BackwardNavigator
   STORE_KEY = 'backward_path'.freeze
 
-  def initialize(storage, backable_paths = ['/'])
+  def initialize(storage, backable_paths = [RequestPath.new('/')])
     @storage = storage
     @storage[STORE_KEY] = backable_paths.first
-    @backable_paths_for_matching = backable_paths.sort_by(&:size).reverse
+    @backable_paths = backable_paths
   end
 
   def update_backward_path(path)
@@ -19,17 +19,7 @@ class BackwardNavigator
 
   private
 
-    def backable_path?(path)
-      if path.include?('?')
-        match_with_query?(path)
-      else
-        @backable_paths_for_matching.include?(path)
-      end
-    end
-
-    def match_with_query?(path)
-      @backable_paths_for_matching.detect do |backable_path|
-        path =~ /#{backable_path}/
-      end
+    def backable_path?(candidate)
+      @backable_paths.detect { |p| p.match?(candidate) }
     end
 end
--- spec/controllers/backward_navigator_spec.rb
+++ spec/controllers/backward_navigator_spec.rb
@@ -10,51 +10,51 @@ describe BackwardNavigator, type: :model do
     let(:navigator) { described_class.new({}, backable_paths) }
 
     context 'register "/", "/tasks" as backable path' do
-      let(:backable_paths) { %w(/ /tasks) }
+      let(:backable_paths) { %w(/ /tasks).map { |p| RequestPath.new(p) } }
 
       context 'visit /' do
-        let(:current_path) { '/' }
-        it { is_expected.to eq('/') }
+        let(:current_path) { RequestPath.new('/') }
+        it { is_expected.to eq(current_path) }
       end
 
       context 'visit /tasks' do
-        let(:current_path) { '/tasks' }
-        it { is_expected.to eq('/tasks') }
+        let(:current_path) { RequestPath.new('/tasks') }
+        it { is_expected.to eq(current_path) }
       end
 
       context 'visit /tasks?tag=ABC' do
-        let(:current_path) { '/tasks?tag=ABC' }
+        let(:current_path) { RequestPath.new('/tasks?tag=ABC') }
         it { is_expected.to eq(current_path) }
       end
 
       context 'visit /tasks/new' do
-        let(:current_path) { '/tasks/new' }
-        it { is_expected.to eq('/') }
+        let(:current_path) { RequestPath.new('/tasks/new') }
+        it { is_expected.to eq(RequestPath.new('/')) }
       end
 
       context 'visit / => /tasks/new' do
         it do
-          navigator.update_backward_path('/')
-          navigator.update_backward_path('/tasks/new')
-          expect(navigator.backward_path).to eq('/')
+          navigator.update_backward_path(RequestPath.new('/'))
+          navigator.update_backward_path(RequestPath.new('/tasks/new'))
+          expect(navigator.backward_path).to eq(RequestPath.new('/'))
         end
       end
 
       context 'visit /tasks => /tasks/new => /tasks/new' do
         it do
-          navigator.update_backward_path('/tasks')
-          navigator.update_backward_path('/tasks/new')
-          navigator.update_backward_path('/tasks/new')
-          expect(navigator.backward_path).to eq('/tasks')
+          navigator.update_backward_path(RequestPath.new('/tasks'))
+          navigator.update_backward_path(RequestPath.new('/tasks/new'))
+          navigator.update_backward_path(RequestPath.new('/tasks/new'))
+          expect(navigator.backward_path).to eq(RequestPath.new('/tasks'))
         end
       end
 
       context 'visit /tasks => / => /tasks/new' do
         it do
-          navigator.update_backward_path('/tasks')
-          navigator.update_backward_path('/')
-          navigator.update_backward_path('/tasks/new')
-          expect(navigator.backward_path).to eq('/')
+          navigator.update_backward_path(RequestPath.new('/tasks'))
+          navigator.update_backward_path(RequestPath.new('/'))
+          navigator.update_backward_path(RequestPath.new('/tasks/new'))
+          expect(navigator.backward_path).to eq(RequestPath.new('/'))
         end
       end
     end
