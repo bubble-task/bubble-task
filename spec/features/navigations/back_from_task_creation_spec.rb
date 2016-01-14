770f77c185e6a867e66b7f6082da382d48703c32
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-01-14 19:10:22 +0900

    refactor

--- spec/features/navigations/back_from_task_creation_spec.rb
+++ spec/features/navigations/back_from_task_creation_spec.rb
@@ -13,19 +13,22 @@ describe 'タスクの作成画面から戻る' do
   let(:task_creation_link) { first('.task-creation-link') }
   let(:back_link) { first('.back-link') }
 
+  let(:invoke_validation_error) { create_task_from_ui_without_visit(title: nil) }
+
   context 'ホーム画面から遷移した場合' do
-    it do
+    before do
       visit root_path
       task_creation_link.click
+    end
+
+    it do
       back_link.click
       expect(current_path).to eq(root_path)
     end
 
     context 'エラーになった場合' do
       it do
-        visit root_path
-        task_creation_link.click
-        create_task_from_ui_without_visit(title: nil)
+        invoke_validation_error
         back_link.click
         expect(current_path).to eq(root_path)
       end
@@ -33,18 +36,19 @@ describe 'タスクの作成画面から戻る' do
   end
 
   context 'タグのタスク一覧から遷移した場合' do
-    it do
+    before do
       visit tasks_path(tag: tag)
       task_creation_link.click
+    end
+
+    it do
       back_link.click
       expect(current_url).to include(tasks_path(tag: tag))
     end
 
     context 'エラーになった場合' do
       it do
-        visit tasks_path(tag: tag)
-        task_creation_link.click
-        create_task_from_ui_without_visit(title: nil)
+        invoke_validation_error
         back_link.click
         expect(current_url).to include(tasks_path(tag: tag))
       end
