a5d44df9094d77c07ccd541773ba5752939cae27
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-01-14 19:05:50 +0900

    タスク編集画面の戻るリンク

--- spec/features/navigations/back_from_task_editing_spec.rb
+++ spec/features/navigations/back_from_task_editing_spec.rb
@@ -20,6 +20,16 @@ describe 'タスクの編集画面から戻る' do
       back_link.click
       expect(current_path).to eq(root_path)
     end
+
+    context 'エラーになった場合' do
+      it do
+        visit root_path
+        task_editing_link.click
+        update_task_from_ui_without_visit(title: 'a' * 100)
+        back_link.click
+        expect(current_path).to eq(root_path)
+      end
+    end
   end
 
   context 'タグのタスク一覧から遷移した場合' do
@@ -29,6 +39,16 @@ describe 'タスクの編集画面から戻る' do
       back_link.click
       expect(current_url).to include(tasks_path(tag: tag))
     end
+
+    context 'エラーになった場合' do
+      it do
+        visit tasks_path(tag: tag)
+        task_editing_link.click
+        update_task_from_ui_without_visit(title: 'a' * 100)
+        back_link.click
+        expect(current_url).to include(tasks_path(tag: tag))
+      end
+    end
   end
 
   context 'タスク詳細画面から遷移した場合' do
@@ -37,6 +57,16 @@ describe 'タスクの編集画面から戻る' do
       task_editing_link.click
       back_link.click
       expect(current_path).to eq(task_path(task))
+    end
+
+    context 'エラーになった場合' do
+      it do
+        visit task_path(task)
+        task_editing_link.click
+        update_task_from_ui_without_visit(title: 'a' * 100)
+        back_link.click
+        expect(current_path).to eq(task_path(task))
+      end
     end
   end
 end
