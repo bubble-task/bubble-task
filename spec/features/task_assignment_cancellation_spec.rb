baf629fef7493002fdfcdebd009e46c3507aa927
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-01-14 12:14:14 +0900

    refactor

--- spec/features/task_assignment_cancellation_spec.rb
+++ spec/features/task_assignment_cancellation_spec.rb
@@ -4,29 +4,25 @@ describe 'タスクのアサインを解除' do
   before do
     sign_in_as(user)
     task
+    TaskAssignment.new(task: task, assignee: user).run
   end
 
   let(:user) { create_user_from_oauth_credential }
   let(:task) { create_task(author_id: user.id, title: 'タスクのタイトル', tags: [tag]) }
   let(:tag) { 'タグ' }
 
-  it do
-    TaskAssignment.new(task: task, assignee: user).run
+  let(:cancel_sign_up_link) { first('.cancel_sign_up') }
+  let(:assigned_avatar) { first(".assignee_#{user.id}") }
 
+  it do
     visit task_path(task.id)
     find('.cancel_sign_up').click
-
-    assigned_avatar = first(".assignee_#{user.id}")
     expect(assigned_avatar).to be_nil
-    cancel_assingment_link = first('.cancel_sign_up')
-    expect(cancel_assingment_link).to be_nil
+    expect(cancel_sign_up_link).to be_nil
   end
 
   it do
-    TaskAssignment.new(task: task, assignee: user).run
-
     visit root_path
-    cancel_assingment_link = first('.cancel_sign_up')
-    expect(cancel_assingment_link).to be_nil
+    expect(cancel_sign_up_link).to be_nil
   end
 end
