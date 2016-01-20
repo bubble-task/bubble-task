c35d72309c5ae62ab0c3ec718a549a1a0e0a1347
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-01-20 18:57:05 +0900

    タスクが複数あるケースを想定して変数名を変更

--- spec/features/task_scheduling_spec.rb
+++ spec/features/task_scheduling_spec.rb
@@ -3,18 +3,18 @@ require 'rails_helper'
 describe 'タスクのスケジューリング' do
   before do
     sign_in_as(user)
-    task
+    task_a
   end
 
   let(:user) { create_user_from_oauth_credential }
-  let(:task) { create_task(author_id: user.id, title: 'タスクのタイトル', tags: ['TAG'], assignees: [user]) }
+  let(:task_a) { create_task(author_id: user.id, title: 'A', tags: ['TAG'], assignees: [user]) }
 
   it do
     visit root_path
     first('.somedays-tasks .move-to-todays-tasks').click
 
     target_task_css_id_in_today = first('.todays-tasks .task-summary')[:id]
-    expect(target_task_css_id_in_today).to eq("task_#{task.id}")
+    expect(target_task_css_id_in_today).to eq("task_#{task_a.id}")
 
     target_task_in_someday = first('.somedays-tasks .task-summary')
     expect(target_task_in_someday).to be_nil
