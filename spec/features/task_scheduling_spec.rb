08bd1836f8546688699ec835f6b9edc7ef6d12ec
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-01-20 18:59:42 +0900

    refactor

--- spec/features/task_scheduling_spec.rb
+++ spec/features/task_scheduling_spec.rb
@@ -3,20 +3,25 @@ require 'rails_helper'
 describe 'タスクのスケジューリング' do
   before do
     sign_in_as(user)
-    task_a
+    tasks
   end
 
   let(:user) { create_user_from_oauth_credential }
   let(:task_a) { create_task(author_id: user.id, title: 'A', tags: ['TAG'], assignees: [user]) }
+  let(:task_b) { create_task(author_id: user.id, title: 'B', tags: ['TAG'], assignees: [user]) }
 
-  it do
-    visit root_path
-    first('.somedays-tasks .move-to-todays-tasks').click
+  context 'タスクが1つしかない場合' do
+    let(:tasks) { [task_a] }
 
-    target_task_css_id_in_today = first('.todays-tasks .task-summary')[:id]
-    expect(target_task_css_id_in_today).to eq("task_#{task_a.id}")
+    it do
+      visit root_path
+      first('.somedays-tasks .move-to-todays-tasks').click
 
-    target_task_in_someday = first('.somedays-tasks .task-summary')
-    expect(target_task_in_someday).to be_nil
+      target_task_css_id_in_today = first('.todays-tasks .task-summary')[:id]
+      expect(target_task_css_id_in_today).to eq("task_#{task_a.id}")
+
+      target_task_in_someday = first('.somedays-tasks .task-summary')
+      expect(target_task_in_someday).to be_nil
+    end
   end
 end
