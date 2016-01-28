46a74e98e8f1b0ccf108851fece3256eb27c3eff
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-01-28 13:40:16 +0900

    refactor

--- app/models/criteria/association_builder.rb
+++ app/models/criteria/association_builder.rb
@@ -12,20 +12,32 @@ module Criteria
     end
 
     def build(conditions)
-      conditions.inject(self) { |b, c| c.prepare(b) }
-
-      @plan_set << { taggings: :left_outer } if @plan_set.empty?
-
-      associations = @plan_set.each_with_object([]) do |plan, a|
-        relation = plan.keys.first.to_s.pluralize
-        join_type = plan.values.first.to_s.upcase.tr('_', ' ')
-        a << "#{join_type} JOIN #{relation} ON #{relation}.task_id = tasks.id"
-      end
-      join_clause = associations.join(' ')
-
-      associated_relations = @plan_set.map { |p| p.keys.first }
-
+      aggregate_plans!(conditions)
+      finalize_plans!
       @relation.joins(join_clause).preload(*associated_relations)
     end
+
+    private
+
+      def aggregate_plans!(conditions)
+        conditions.inject(self) { |b, c| c.prepare(b) }
+      end
+
+      def finalize_plans!
+        @plan_set << { taggings: :left_outer } if @plan_set.empty?
+      end
+
+      def join_clause
+        associations = @plan_set.each_with_object([]) do |plan, a|
+          relation = plan.keys.first.to_s.pluralize
+          join_type = plan.values.first.to_s.upcase.tr('_', ' ')
+          a << "#{join_type} JOIN #{relation} ON #{relation}.task_id = tasks.id"
+        end
+        join_clause = associations.join(' ')
+      end
+
+      def associated_relations
+        @plan_set.map { |p| p.keys.first }
+      end
   end
 end
