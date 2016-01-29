8e418d2461a07dd4f248172d9803256d6aa168f0
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-01-29 13:24:38 +0900

    generate sql by association

--- app/models/criteria/association_plan.rb
+++ app/models/criteria/association_plan.rb
@@ -1,6 +1,4 @@
 module Criteria
-  Association = Struct.new(:left_relation, :join_type, :right_relation)
-
   AssociationPlan = Struct.new(:relation, :join_type) do
 
     def associate(base_relation)
