36c26b422002a7ec8a5029e138e9f36df92852f7
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-02-09 18:16:06 +0900

    完了タスクのみ検索するためのCriteria::CompletedTaskを追加

--- app/models/criteria/completed_task.rb
+++ app/models/criteria/completed_task.rb
@@ -1,27 +1,11 @@
 module Criteria
-  class Task
+  class CompletedTask
 
     def self.create(assignee_id: nil, from_date: nil, to_date: nil, tag_words: nil, completion_state: nil)
-      if completion_state == 'completed'
-        return Criteria::CompletedTask.create(
-          assignee_id: assignee_id,
-          from_date: from_date,
-          to_date: to_date,
-          tag_words: tag_words,
-          completion_state: completion_state,
-        )
-      end
-
       new.tap do |c|
         c.add_condition(Criteria::Conditions::Assignee.create(assignee_id))
-        case completion_state
-        when 'uncompleted'
-          c.add_condition(Criteria::Conditions::DeadlineFrom.create(from_date))
-          c.add_condition(Criteria::Conditions::DeadlineTo.create(to_date))
-        when 'any'
-          c.add_condition(Criteria::Conditions::CompletedOnFrom.create(from_date))
-          c.add_condition(Criteria::Conditions::CompletedOnTo.create(to_date))
-        end
+        c.add_condition(Criteria::Conditions::CompletedOnFrom.create(from_date))
+        c.add_condition(Criteria::Conditions::CompletedOnTo.create(to_date))
         c.add_condition(Criteria::Conditions::Tags.create(tag_words))
         c.add_condition(Criteria::Conditions::Completion.create(completion_state))
       end
