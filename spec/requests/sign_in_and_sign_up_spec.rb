fb3e2f1d867a16e074488e20f6fcb0fb6d93ac1d
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2016-02-10 15:58:18 +0900

    ログイン機能のspecのフィクスチャをリファクタリング

--- spec/requests/sign_in_and_sign_up_spec.rb
+++ spec/requests/sign_in_and_sign_up_spec.rb
@@ -2,62 +2,43 @@ require 'rails_helper'
 
 describe 'GET /auth/google/callback' do
   context 'OAuth認証に成功した場合' do
-    context 'gaiax.comの場合' do
-      it 'ログイン状態になっていること' do
-        OmniAuth.config.add_mock(
-          :google,
-          {
-            'provider' => 'google',
-            'uid' => '1234567890',
-            'info' => {
-              'email' => 'user@gaiax.com',
-              'name' => 'ユーザの名前',
-            },
+    before do
+      OmniAuth.config.add_mock(
+        :google,
+        {
+          'provider' => 'google',
+          'uid' => '1234567890',
+          'info' => {
+            'email' => email,
+            'name' => 'ユーザの名前',
           },
-        )
-        Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
-        get(oauth_callbacks_path(provider: :google))
-        follow_redirect!
+        },
+      )
+      Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
+      get(oauth_callbacks_path(provider: :google))
+      follow_redirect!
+    end
+
+    context 'gaiax.comの場合' do
+      let(:email) { 'user@gaiax.com' }
+
+      it 'ログイン状態になっていること' do
         expect(response.body).to include('ユーザの名前')
       end
     end
 
     context 'adish.co.jpの場合' do
+      let(:email) { 'user@adish.co.jp' }
+
       it 'ログイン状態になっていること' do
-        OmniAuth.config.add_mock(
-          :google,
-          {
-            'provider' => 'google',
-            'uid' => '1234567890',
-            'info' => {
-              'email' => 'user@adish.co.jp',
-              'name' => 'ユーザの名前',
-            },
-          },
-        )
-        Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
-        get(oauth_callbacks_path(provider: :google))
-        follow_redirect!
         expect(response.body).to include('ユーザの名前')
       end
     end
 
     context 'gmail.comの場合' do
+      let(:email) { 'user@gmail.com' }
+
       it '登録エラーになっていること' do
-        OmniAuth.config.add_mock(
-          :google,
-          {
-            'provider' => 'google',
-            'uid' => '1234567890',
-            'info' => {
-              'email' => 'user@gmai.com',
-              'name' => 'ユーザの名前',
-            },
-          },
-        )
-        Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
-        get(oauth_callbacks_path(provider: :google))
-        follow_redirect!
         expect(response.body).to include('お使いのメールアドレスではご利用いただけません')
       end
     end
