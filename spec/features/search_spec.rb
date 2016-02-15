bcb038f226a530ed3486898c995212b307de7b53
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-02-15 10:50:17 +0900

    タスク検索結果に未完了タスクの期限を表示する

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -20,7 +20,8 @@ describe 'タスクの検索' do
     create_task(
       author_id: user_a.id, title: '未完了',
       tags: %w(DEF),
-      assignees: [user_a, user_b]
+      assignees: [user_a, user_b],
+      deadline: Time.current,
     )
   end
 
@@ -49,6 +50,46 @@ describe 'タスクの検索' do
       click_button(I18n.t('search.index.actions.search'))
       tasks = all('.task-summary-primary')
       expect(tasks.size).to eq(2)
+    end
+  end
+
+  describe '検索結果の表示' do
+    context '完了タスク' do
+      before do
+        completed_task
+        visit search_path
+        click_button(I18n.t('search.index.actions.search'))
+      end
+
+      it do
+        completed_on_page = first('.task-completed-on').text
+        completed_on = TaskPresenter.new(completed_task).completed_on
+        expect(completed_on_page).to eq(I18n.l(completed_on, format: :default))
+      end
+
+      it do
+        cancel_completion = first('.cancel-completion')
+        expect(cancel_completion).to_not be_nil
+      end
+    end
+
+    context '未完了タスク' do
+      before do
+        uncompleted_task
+        visit search_path
+        click_button(I18n.t('search.index.actions.search'))
+      end
+
+      it do
+        deadline_on_page = first('.task-completed-on').text
+        deadline = uncompleted_task.deadline
+        expect(deadline_on_page).to eq(I18n.l(deadline, format: :short))
+      end
+
+      it do
+        cancel_completion = first('.cancel-completion')
+        expect(cancel_completion).to be_nil
+      end
     end
   end
 end
