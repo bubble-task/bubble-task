0f8500ba1571fd7f1a9ac9272db8a05c6be81fa1
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-02-16 15:41:35 +0900

    今日のタスクを操作するアクション

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,16 +6,4 @@ class HomeController < ApplicationController
       TaskPresenter.new(task)
     end
   end
-
-  def create_todays_task
-    #todays_tasks = TodaysTaskList.load(current_user.id)
-    #TaskScheduling.new(params[:task_id], todays_tasks).run
-    redirect_to root_url
-  end
-
-  def destroy_todays_task
-    #todays_tasks = TodaysTaskList.load(current_user.id)
-    #TaskCancellationScheduling.new(params[:task_id], todays_tasks).run
-    redirect_to root_url
-  end
 end
