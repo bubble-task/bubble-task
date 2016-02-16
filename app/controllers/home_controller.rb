d4a21138c35c756366850927a8a04cf14fd57d3d
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-02-16 15:36:44 +0900

    自分のタスクのみ取得

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,8 @@ class HomeController < ApplicationController
   before_action :authorize!
 
   def index
-    tasks = TaskRepository.all_uncompleted_by_assignee(current_user.id).map do |task|
+    @tasks = TaskRepository.all_uncompleted_by_assignee(current_user.id).map do |task|
       TaskPresenter.new(task)
-    end
-    tasks.group_by { |t| t.todays_task_for_user?(current_user.id) }.tap do |group|
-      @todays_tasks = Array(group[true])
-      @somedays_tasks = Array(group[false])
     end
   end
 
