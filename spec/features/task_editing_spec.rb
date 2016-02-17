32d09e95c5134b71a8940fa58ea437721c2d72ee
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2016-02-17 11:16:43 +0900

    個人タスクだったものを通常のタスクに変更する場合のテストを追加

--- spec/features/task_editing_spec.rb
+++ spec/features/task_editing_spec.rb
@@ -100,6 +100,18 @@ describe 'タスクの編集' do
         expect(description_on_page).to eq(old_description)
         expect(tags_on_page).to eq(%w(タグ1 タグ2 タグ3))
       end
+
+      context '個人タスクにタグを追加' do
+        let(:old_tags) { [] }
+
+        it do
+          update_task_from_ui(task, tag_words: new_tag_words)
+          click_link old_title
+          expect(title_on_page).to eq(old_title)
+          expect(description_on_page).to eq(old_description)
+          expect(tags_on_page).to eq(%w(タグ1 タグ2 タグ3))
+        end
+      end
     end
 
     context 'タグを1つ削除' do
@@ -118,7 +130,7 @@ describe 'タスクの編集' do
       let(:new_tag_words) { '' }
 
       context '自分が作成したタスク' do
-        it do
+        it '自分の個人タスクになること' do
           update_task_from_ui(task, tag_words: new_tag_words)
           click_link old_title
           expect(title_on_page).to eq(old_title)
@@ -131,7 +143,7 @@ describe 'タスクの編集' do
         let(:other_user) { create_user_from_oauth_credential(generate_auth_hash(email: 'other_user@gaiax.com')) }
         let(:task) { create_task(author_id: other_user.id, title: old_title, tags: old_tags, assignees: [other_user]) }
 
-        it do
+        it '自分の個人タスクになること' do
           update_task_from_ui(task, tag_words: new_tag_words)
           expect(title_on_page).to eq(old_title)
           expect(tags_on_page).to eq(%w(個人タスク))
@@ -142,7 +154,7 @@ describe 'タスクの編集' do
         let(:other_user) { create_user_from_oauth_credential(generate_auth_hash(email: 'other_user@gaiax.com')) }
         let(:task) { create_task(author_id: other_user.id, title: old_title, tags: old_tags, assignees: [user]) }
 
-        it do
+        it '編集した他人の個人タスクになること' do
           command = TaskEditing.new(task, TaskEditingForm.new({ title: 'タイトル', tag_words: '' }.merge(task_id: task.id)))
           command.run(other_user)
           visit root_path
