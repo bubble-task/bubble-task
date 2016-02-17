c7291c88076ca2607bd6e9cacb642cc15e57ce0c
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-02-17 16:01:18 +0900

    wip

--- spec/requests/personal_task_search_spec.rb
+++ spec/requests/personal_task_search_spec.rb
@@ -75,4 +75,27 @@ describe 'GET /search' do
       expect(tasks).to eq(expected_tasks)
     end
   end
+
+  skip '期間指定なし&タグ指定なし&未完了のみ&自分がサインアップONで検索 ' do
+    let(:expected_tasks) do
+      [
+        create_personal_task(user: user, title: '個人タスク'),
+        create_task(author_id: user.id, title: '公開タスク1', tags: %w(TagA), assignees: [user]),
+      ]
+    end
+
+    let(:unexpected_tasks) do
+      [
+        create_personal_task(user: other_user, title: '他人の個人タスク'),
+        create_personal_task(user: user, title: '完了個人タスク', completed_at: :now),
+        create_task(author_id: other_user.id, title: '公開タスク3', tags: %w(TagA TagC), assignees: [user, other_user], completed_at: '2016/02/02'),
+      ]
+    end
+
+    it do
+      get search_path(c: { is_signed_up_only: '1', completion_state: 'uncompleted' })
+      tasks = assigns(:tasks)
+      expect(tasks).to eq(expected_tasks)
+    end
+  end
 end
