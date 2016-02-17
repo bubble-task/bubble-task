905aafff16594b8c3ed18a8d07b78ddcbc077879
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2016-02-17 12:44:25 +0900

    TaskCreationコマンドをリファクタリング

--- app/commands/task_creation.rb
+++ app/commands/task_creation.rb
@@ -7,14 +7,28 @@ class TaskCreation
 
   def run(user)
     return false unless @form.valid?
-    task = TaskFactory.create(user.id, @form.title, @form.description, @form.tags, @form.deadline)
-    if @form.tags.empty?
-      task.to_personal_task(user)
-    end
-    task.save!
-    if @form.with_sign_up?
-      TaskAssignment.new(task: task, assignee: user).run
-    end
+    task = create_task(user)
+    sign_up!(task, user)
     task
   end
+
+  private
+
+    def create_task(user)
+      task = TaskFactory.create(user.id, @form.title, @form.description, @form.tags, @form.deadline)
+      task = to_personal_task!(task, user)
+      task.save!
+      task
+    end
+
+    def to_personal_task!(task, user)
+      return task if @form.tags.any?
+      task.to_personal_task(user)
+      task
+    end
+
+    def sign_up!(task, user)
+      return unless @form.with_sign_up?
+      TaskAssignment.new(task: task, assignee: user).run
+    end
 end
