e3360f394ce905e948b410103ff69d81cfa89a67
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-02-23 18:50:46 +0900

    refactor

--- spec/features/task_completion_spec.rb
+++ spec/features/task_completion_spec.rb
@@ -27,25 +27,46 @@ describe 'タスクの完了', js: true do
     end
   end
 
+  shared_examples_for 'チェックボックスが表示されていないこと' do
+    it 'チェックボックスが表示されていないこと' do
+      completion_checkbox = first(completed_checkbox_label_id)
+      expect(completion_checkbox).to be_nil
+    end
+  end
+
+  shared_examples_for 'タスクが完了状態になっていること' do
+    it 'タスクが非表示になっていること' do
+      find(completed_checkbox_label_id, visible: false).click
+      wait_completion
+      expect(title_link).to be_nil
+    end
+
+    it '一覧画面に再度アクセスするとタスクが非表示になっていること' do
+      find(completed_checkbox_label_id, visible: false).click
+      visit root_path
+      expect(title_link).to be_nil
+    end
+  end
+
+  shared_examples_for '完了にチェックが入っていること' do
+    it do
+      find(completed_checkbox_label_id, visible: false).click
+      wait_completion
+      expect(completed_checkbox).to be_checked
+    end
+  end
+
   describe '公開タスク:自分がサインアップしていないタスクを完了にする' do
     let(:task) { create_task(author_id: user_a.id, title: title, tags: [tag], assignees: [user_b]) }
 
     context 'タグのタスク一覧画面で操作する場合' do
       before { visit tasks_path(tag: tag) }
-
-      it 'チェックボックスが表示されていないこと' do
-        completion_checkbox = first(completed_checkbox_label_id)
-        expect(completion_checkbox).to be_nil
-      end
+      it_behaves_like 'チェックボックスが表示されていないこと'
     end
 
     context 'タスク詳細画面で操作する場合' do
       before { visit task_path(task.id) }
-
-      it 'チェックボックスが表示されていないこと' do
-        completion_checkbox = first(completed_checkbox_label_id)
-        expect(completion_checkbox).to be_nil
-      end
+      it_behaves_like 'チェックボックスが表示されていないこと'
     end
   end
 
@@ -54,11 +75,7 @@ describe 'タスクの完了', js: true do
 
     context 'タグのタスク一覧画面で操作する場合' do
       before { visit tasks_path(tag: tag) }
-
-      it 'チェックボックスが表示されていないこと' do
-        completion_checkbox = first(completed_checkbox_label_id)
-        expect(completion_checkbox).to be_nil
-      end
+      it_behaves_like 'チェックボックスが表示されていないこと'
     end
   end
 
@@ -67,28 +84,12 @@ describe 'タスクの完了', js: true do
 
     context 'ホーム画面で操作する場合' do
       before { visit root_path }
-
-      it 'タスクが非表示になっていること' do
-        find(completed_checkbox_label_id, visible: false).click
-        wait_completion
-        expect(title_link).to be_nil
-      end
-
-      it '一覧画面に再度アクセスするとタスクが非表示になっていること' do
-        find(completed_checkbox_label_id, visible: false).click
-        visit root_path
-        expect(title_link).to be_nil
-      end
+      it_behaves_like 'タスクが完了状態になっていること'
     end
 
     context 'タスク詳細画面で操作する場合' do
       before { visit task_path(task.id) }
-
-      it do
-        find(completed_checkbox_label_id, visible: false).click
-        wait_completion
-        expect(completed_checkbox).to be_checked
-      end
+      it_behaves_like '完了にチェックが入っていること'
     end
   end
 
@@ -97,72 +98,29 @@ describe 'タスクの完了', js: true do
 
     context 'ホーム画面で操作する場合' do
       before { visit root_path }
-
-      it 'タスクが非表示になっていること' do
-        find(completed_checkbox_label_id, visible: false).click
-        wait_completion
-        expect(title_link).to be_nil
-      end
-
-      it '一覧画面に再度アクセスするとタスクが非表示になっていること' do
-        find(completed_checkbox_label_id, visible: false).click
-        visit root_path
-        expect(title_link).to be_nil
-      end
+      it_behaves_like 'タスクが完了状態になっていること'
     end
 
     context 'タスク詳細画面で操作する場合' do
       before { visit task_path(task.id) }
-
-      it do
-        find(completed_checkbox_label_id, visible: false).click
-        wait_completion
-        expect(completed_checkbox).to be_checked
-      end
+      it_behaves_like '完了にチェックが入っていること'
     end
   end
 
   describe '公開タスク:自分がサインアップ済みのタスクを完了にする' do
     context 'ホーム画面で操作する場合' do
       before { visit root_path }
-
-      it 'タスクが非表示になっていること' do
-        find(completed_checkbox_label_id, visible: false).click
-        wait_completion
-        expect(title_link).to be_nil
-      end
-
-      it '一覧画面に再度アクセスするとタスクが非表示になっていること' do
-        find(completed_checkbox_label_id, visible: false).click
-        visit root_path
-        expect(title_link).to be_nil
-      end
+      it_behaves_like 'タスクが完了状態になっていること'
     end
 
     context 'タグのタスク一覧画面で操作する場合' do
       before { visit tasks_path(tag: tag) }
-
-      it 'タスクが非表示になっていること' do
-        find(completed_checkbox_label_id, visible: false).click
-        wait_completion
-        expect(title_link).to be_nil
-      end
-
-      it '一覧画面に再度アクセスするとタスクが非表示になっていること' do
-        find(completed_checkbox_label_id, visible: false).click
-        visit tasks_path(tag: tag)
-        expect(title_link).to be_nil
-      end
+      it_behaves_like 'タスクが完了状態になっていること'
     end
 
     context 'タスク詳細画面で操作する場合' do
       before { visit task_path(task.id) }
-
-      it do
-        find(completed_checkbox_label_id, visible: false).click
-        wait_completion
-        expect(completed_checkbox).to be_checked
-      end
+      it_behaves_like '完了にチェックが入っていること'
     end
   end
 end
