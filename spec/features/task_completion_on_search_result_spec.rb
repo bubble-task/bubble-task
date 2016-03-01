7ce3b41ee3cd9cd30651636c8ac3b0d571531e08
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-03-01 19:04:39 +0900

    サインアップしていないタスクは完了チェックボックスを表示しない

--- spec/features/task_completion_on_search_result_spec.rb
+++ spec/features/task_completion_on_search_result_spec.rb
@@ -7,20 +7,36 @@ describe 'タスク検索結果から完了にする', js: true do
   end
 
   let(:user_a) { create_user_from_oauth_credential }
+  let(:user_b) { create_user_from_oauth_credential }
 
   let(:wait_completion) { find('#toast-container') }
   let(:completed_checkbox_id) { "#task_#{task.id}_completion_check" }
   let(:completed_checkbox) { find(completed_checkbox_id, visible: false) }
   let(:completed_checkbox_label_id) { "#task_#{task.id}_completion_mark" }
 
-  let(:task) { create_task(author_id: user_a.id, title: '公開タスク1', tags: %w(Tag), assignees: [user_a]) }
+  context 'サインアップしている場合' do
+    let(:task) { create_task(author_id: user_a.id, title: '公開タスク', tags: %w(Tag), assignees: [user_a]) }
 
-  it do
-    visit search_path
-    select '完了/未完了どちらも', from: 'c[completion_state]'
-    click_button(I18n.t('search.index.actions.search'))
-    find(completed_checkbox_label_id, visible: false).click
-    wait_completion
-    expect(completed_checkbox).to be_checked
+    it do
+      visit search_path
+      select '完了/未完了どちらも', from: 'c[completion_state]'
+      click_button(I18n.t('search.index.actions.search'))
+      find(completed_checkbox_label_id, visible: false).click
+      wait_completion
+      expect(completed_checkbox).to be_checked
+    end
+  end
+
+  context 'サインアップしていない場合' do
+    let(:task) { create_task(author_id: user_a.id, title: '公開タスク', tags: %w(Tag), assignees: [user_b]) }
+
+    it do
+      visit search_path
+      select '完了/未完了どちらも', from: 'c[completion_state]'
+      find('#c_is_signed_up_only_label', visible: false).click
+      click_button(I18n.t('search.index.actions.search'))
+      completion_checkbox = first(completed_checkbox_label_id)
+      expect(completion_checkbox).to be_nil
+    end
   end
 end
