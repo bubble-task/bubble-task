9884fae869b1aecff9d7cc5d42aeb3d31d245a26
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2016-03-02 11:20:28 +0900

    未完了タスクを検索した結果のタスクを完了にする時のテストを追加

--- spec/features/task_completion_on_search_result_spec.rb
+++ spec/features/task_completion_on_search_result_spec.rb
@@ -17,24 +17,32 @@ describe 'タスク検索結果から完了にする', js: true do
   context 'サインアップしている場合' do
     let(:task) { create_task(author_id: user_a.id, title: '公開タスク', tags: %w(Tag), assignees: [user_a]) }
 
-    it do
-      visit search_path
-      select '完了/未完了どちらも', from: 'c[completion_state]'
-      click_button(I18n.t('search.index.actions.search'))
-      find(completed_checkbox_label_id, visible: false).click
-      wait_completion
-      expect(completed_checkbox).to be_checked
+    context '完了/未完了を問わない場合' do
+      it do
+        visit search_path(c: { completion_state: 'any', is_signed_up_only: '1' })
+        find(completed_checkbox_label_id, visible: false).click
+        wait_completion
+        expect(completed_checkbox).to be_checked
+      end
+    end
+
+    context '未完了タスクのみの場合' do
+      it do
+        visit search_path(c: { completion_state: 'uncompleted', is_signed_up_only: '1' })
+        find(completed_checkbox_label_id, visible: false).click
+        wait_completion
+        expect(first(completed_checkbox_id)).to be_nil
+        visit search_path(c: { completion_state: 'completed', is_signed_up_only: '1' })
+        expect(completed_checkbox).to_not be_nil
+      end
     end
   end
 
-  context 'サインアップしていない場合' do
+  context 'サインアップしていない場合', js: false do
     let(:task) { create_task(author_id: user_a.id, title: '公開タスク', tags: %w(Tag), assignees: [user_b]) }
 
     it do
-      visit search_path
-      select '完了/未完了どちらも', from: 'c[completion_state]'
-      find('#c_is_signed_up_only_label', visible: false).click
-      click_button(I18n.t('search.index.actions.search'))
+      visit search_path(c: { completion_state: 'any', is_signed_up_only: '0' })
       completion_checkbox = first(completed_checkbox_label_id)
       expect(completion_checkbox).to be_nil
     end
