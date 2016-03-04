ffbbda89cf607caa66dcac4a27f32b60d98e652a
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2016-03-04 11:57:44 +0900

    ホーム画面のタスクの並び順のテストをリファクタリング

--- spec/features/task_sorting_spec.rb
+++ spec/features/task_sorting_spec.rb
@@ -5,28 +5,39 @@ describe 'タスクの並び順' do
 
   before do
     sign_in_as(user)
+    tasks
   end
 
   let(:user) { create_user_from_oauth_credential }
   let(:create_task_for_user_b) { create_task(author_id: user_b.id, title: 'ユーザBのタスク') }
+  let(:tasks) do
+    [
+      create_task(author_id: user.id, title: '期限なし,作成順1'),
+      create_task(author_id: user.id, title: '期限なし,作成順2'),
+      create_task(author_id: user.id, title: '期限なし,作成順3'),
+      create_task(author_id: user.id, title: '期限=2016-01-03', deadline: Time.zone.parse('2016-01-03')),
+      create_task(author_id: user.id, title: '期限=2016-01-01', deadline: Time.zone.parse('2016-01-01')),
+      create_task(author_id: user.id, title: '期限=2016-01-02', deadline: Time.zone.parse('2016-01-02')),
+    ]
+  end
 
   context 'ホーム画面' do
+    let(:expected_task_css_ids) do
+      [
+        task_css_id(Task.find_by(title: '期限=2016-01-01').id),
+        task_css_id(Task.find_by(title: '期限=2016-01-02').id),
+        task_css_id(Task.find_by(title: '期限=2016-01-03').id),
+        task_css_id(Task.find_by(title: '期限なし,作成順1').id),
+        task_css_id(Task.find_by(title: '期限なし,作成順2').id),
+        task_css_id(Task.find_by(title: '期限なし,作成順3').id),
+      ]
+    end
+
+    let(:task_css_ids) { all('.task-summary').map { |t| t[:id] } }
+
     it do
-      task_without_deadline4 = create_task(author_id: user.id, title: '期限なしタスク4')
-      task_without_deadline5 = create_task(author_id: user.id, title: '期限なしタスク5')
-      task_without_deadline6 = create_task(author_id: user.id, title: '期限なしタスク6')
-      task_with_deadline3 = create_task(author_id: user.id, title: '期限付きタスク3', deadline: Time.zone.parse('2016-01-03'))
-      task_with_deadline1 = create_task(author_id: user.id, title: '期限付きタスク1', deadline: Time.zone.parse('2016-01-01'))
-      task_with_deadline2 = create_task(author_id: user.id, title: '期限付きタスク2', deadline: Time.zone.parse('2016-01-02'))
       visit root_path
-      expect(all('.task-summary').map { |t| t[:id] }).to eq([
-        task_css_id(task_with_deadline1.id),
-        task_css_id(task_with_deadline2.id),
-        task_css_id(task_with_deadline3.id),
-        task_css_id(task_without_deadline4.id),
-        task_css_id(task_without_deadline5.id),
-        task_css_id(task_without_deadline6.id),
-      ])
+      expect(task_css_ids).to eq(expected_task_css_ids)
     end
   end
 end
