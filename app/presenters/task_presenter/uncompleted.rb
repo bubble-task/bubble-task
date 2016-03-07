2df26168aa39eb13d9793d3dc70925940d6528ff
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2016-03-07 18:16:51 +0900

    タスクの完了、未完了のチェックボックスの表示部分をすべてPresenterに委譲

--- app/presenters/task_presenter/uncompleted.rb
+++ app/presenters/task_presenter/uncompleted.rb
@@ -23,6 +23,7 @@ module TaskPresenter
     end
 
     def completion_form(view)
+      return "<p></p>".html_safe unless can_complete?(view.current_user.id)
       view.form_for(completion_command, url: view.complete_task_url(self), method: :put, remote: true) do |f|
         completion_form_fields(self)
       end
